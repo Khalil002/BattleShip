f24fab5e1106942f94fb913ce2ced05aa5163695
K-lil001 <90007227+Khalil002@users.noreply.github.com>
2022-04-03 20:10:51 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,15 +37,32 @@ class Bot(Player):
             x = self.hitX[0]
             y = self.hitY[0]
             if(x-1 > -1 and enemy.board[x-1][y] < 2):
+                print("AT 00000000")
+                print(self.boardSize)
+                print(x-1)
+                print(y)
                 x = x-1
                 result = super().attack(x, y, enemy)
             elif(x+1 < self.boardSize and enemy.board[x+1][y] < 2):
+                print("AT 111111111")
+                print(self.boardSize)
+                print(x+1)
+                print(y)
                 x = x+1
                 result = super().attack(x, y, enemy)
             elif(y-1 > -1 and enemy.board[x][y-1] < 2):
+                print("AT 2222222222")
+                print(self.boardSize)
+                print(x)
+                print(y-1)
+                
                 y = y-1
                 result = super().attack(x, y, enemy)
             elif(y+1 < self.boardSize):
+                print("AT 3333333333")
+                print(self.boardSize)
+                print(x)
+                print(y+1)
                 y = y+1
                 result = super().attack(x, y, enemy)
         else:
@@ -59,15 +76,28 @@ class Bot(Player):
                         found = True
                         break
                 if(found):
+                    print("AT 4444444444")
+                    print(self.boardSize)
+                    print(x)
+                    print(y)
                     result = super().attack(x, y, enemy)
+                    
                 else:
                     found2 = False
                     for i in range(len(self.hitX)):
                         x = self.hitX[i]+1
                         if(x < self.boardSize and enemy.board[x][y] < 2):
+                            found2= True
                             break
+                    if(found2):
+                        print("AT 5555555555")
+                        print(self.boardSize)
+                        print(x)
+                        print(y)
+                        result = super().attack(x, y, enemy)
+
                     
-                    result = super().attack(x, y, enemy)
+                    
                         
             else:
                 #Bot finds out the ship is in rotation 1
@@ -79,15 +109,26 @@ class Bot(Player):
                         found = True
                         break
                 if(found):
+                    print("AT 6666666665")
+                    print(self.boardSize)
+                    print(x)
+                    print(y)
                     result = super().attack(x, y, enemy)
+                    
                 else:
                     found2 = False
                     for i in range(len(self.hitY)):
                         y = self.hitY[i]+1
                         if(y < self.boardSize and enemy.board[x][y] < 2):
+                            found2 = True
                             break
                     
-                    result = super().attack(x, y, enemy)
+                    if(found2):
+                        print("AT 777777777777")
+                        print(self.boardSize)
+                        print(x)
+                        print(y)
+                        result = super().attack(x, y, enemy)
         
         if(result == 2):
             self.hitX.append(x)
